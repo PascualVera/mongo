183173ac4a245800ec39621abc5f7b46b4ac9fe0
Germán Abenza López <54044544+GermanAbenza@users.noreply.github.com>
2022-02-27 20:51:59 +0100

    example (#1)

--- imdbMongo/src/Front/Fetch/profesionalFetch.js
+++ imdbMongo/src/Front/Fetch/profesionalFetch.js
@@ -1,65 +1,60 @@
 //Declaracion de variables
-let name = document.getElementById("name")
-let age = document.getElementById("age")
-let genre = document.getElementById("genre")
-let weight = document.getElementById("weight")
-let height = document.getElementById("height")
-let hair = document.getElementById("hair")
-let race = document.getElementById("race")
-let active = document.getElementById("active")
-let nacionality = document.getElementById("nationality")
-let oscars = document.getElementById("oscars")
-let profesion = document.getElementById("profession")
-let img = document.getElementById("img")
-let idNumber = document.querySelector("#idNumber")
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let genre = document.getElementById("genre");
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let hair = document.getElementById("hair");
+let race = document.getElementById("race");
+let active = document.getElementById("active");
+let nacionality = document.getElementById("nationality");
+let oscars = document.getElementById("oscars");
+let profesion = document.getElementById("profession");
+let img = document.getElementById("img");
+let idNumber = document.querySelector("#idNumber");
 //Botones
-let mostrarBtn = document.querySelector("#btn-get")
-let crearBtn = document.querySelector("#btn-post")
-let actualizarBtn = document.querySelector("#btn-put")
-let eliminarBtn = document.querySelector("#btn-del")
+let mostrarBtn = document.querySelector("#btn-get");
+let crearBtn = document.querySelector("#btn-post");
+let actualizarBtn = document.querySelector("#btn-put");
+let eliminarBtn = document.querySelector("#btn-del");
 
 mostrarBtn.addEventListener("click", async () => {
-	let id = Number(idNumber.value)
-	let url = "http://localhost:3000/profesional"
+  let id = Number(idNumber.value);
+  let url = "http://localhost:3000/profesional";
 
-	let param = {
-		headers: {
-			"content-type": "application/json; charset = UTF-8",
-		},
-		method: "GET",
-	}
-	try {
-		let data = await fetch(url, param)
-		let result = await data.json()
-		if (idNumber.value == "") {
-			document.querySelector(".database").innerHTML = mostrarTodos(result)
-		}
-	} catch (error) {
-		console.log(error)
-	}
-})
+  let param = {
+    headers: {
+      "content-type": "application/json; charset = UTF-8",
+    },
+    method: "GET",
+  };
+  try {
+    let data = await fetch(url, param);
+    let result = await data.json();
+    if (idNumber.value == "") {
+      document.querySelector(".database").innerHTML = mostrarTodos(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const mostrarTodos = (database) => {
-	let post = ""
-	for (const actor of database) {
-		post += `<button value=${actor._id} onClick=polla() class=actor>
+  let post = "";
+  for (const actor of database) {
+    post += `<button value=${actor._id} onClick=polla() class=actor>
 		<span>Nombre: ${actor.name}</span>
 		<span>Profesion: ${actor.profesion} </span>
 		</button>
-		`
-	}
-	return post
-}
-let button = document.querySelectorAll(".actor")
-function polla(e) {
-	console.log(e.target)
-	for (const object of button) {
-		console.log(object.value)
-	}
-}
-// setTimeout(() => {
-// 	document.querySelector(".database").addEventListener("click", (e) => {
-// 		e.stopPropagation()
-// 		console.log(this.target)
-// 	})
-// }, 2000)
+		`;
+  }
+  return post;
+};
+
+const listaActores = document.querySelector(".database");
+listaActores.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const element = e.target;
+  const parent = element.parentElement;
+  console.log(parent.getAttribute("value"));
+});
